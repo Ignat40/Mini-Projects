737f86c494008949aa60beab85c6cc962d039b74
Ignat Bojinov <ignatbojinov@gmail.com>
2023-10-29 11:20:16 +0100

    start quiz game proj

---
+++ quiz_game/quiz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+
+namespace Quiz_Game
+{
+    enum QuizCategories
+    {
+        GeneralKnowladge,
+        History,
+        Biology,
+        Mathematic,
+        Sport,
+
+    }
+    struct QuizQuestions
+    {
+        public string Question;
+        public int Score; 
+        public int CorrectAnswerIndex;
+        public string[] QuestionOptions;
+        public QuizCategories Categories; 
+    }
+    public class Program
+    {
+        /// <summary>
+        /// Create an quiz game using C# that utilizes enums,
+        /// structs, loops, and functions. The program should 
+        /// allow users to answer quiz questions, track their
+        /// score, and display the final score at the end of the quiz.
+        /// </summary>
+        public static void Main()
+        {
+            
+        }
+        public static void DisplayCategories()
+        {
+
+        }
+        public static void LoadQuestions()
+        {
+
+        }
+        public static void PlayQuiz()
+        {
+
+        }
+        public static QuizCategories ChooseCategorie()
+        {
+
+        }
+    }
+}
