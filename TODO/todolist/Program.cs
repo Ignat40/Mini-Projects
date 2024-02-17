21a0c3a4c2a291e31f06758a0f4a6a255e84f6b9
Ignat Bojinov <ignatbojinov@gmail.com>
2024-02-17 15:13:50 +0100

    start to do list

---
+++ TODO/todolist/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("|       To Do List      |");
+            Console.WriteLine("-------------------------");
+
+            View view = new();
+            AddTask addTask = new();
+            addTask.TakeAndDisplay();
+            view.List();
+        }
+    }
+}
